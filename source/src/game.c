19ce604bf46132989a9989b9655860d2f8f0bad6
Nils Daumann <nils_daumann@slindev.com>
2015-10-18 01:17:22 +0200

    SHADOWS!!!!!!

--- source/src/game.c
+++ source/src/game.c
@@ -10,6 +10,8 @@ void startGame()
 	playMusicGameDay();
 	on_space = throwSnowball;
 	
+	sun_light = 100;
+	
 	random_seed(8);
 	
 	VECTOR tempVector;
@@ -32,10 +34,12 @@ void startGame()
 		}
 		
 		ENTITY *tree = ent_create("models//pine_3.mdl", tempVector, 0);
-		set(tree, PASSABLE);
+		set(tree, PASSABLE|SHADOW);
 		vec_scale(tree.scale_x, random(0.5) + 0.5);
 		tree.pan = random(360);
 	}
+	
+	pssm_run(4);
 
 	while(1)
 	{
