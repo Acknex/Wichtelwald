70bec916e83baae5d84d6481b62564706f56011a
firoball <unsinnspala@firoball.de>
2015-10-18 02:35:06 +0200

    hut code

--- source/include/hut.h
+++ source/include/hut.h
@@ -1,4 +1,7 @@
 #ifndef HUT_H
 #define HUT_H
 
+#define is_empty  FLAG4
+#define HIT_TIME 0.5
+
 #endif
--- source/src/hut.c
+++ source/src/hut.c
@@ -1,9 +1,23 @@
 #ifndef HUT_C
 #define HUT_C
 
+var hutSnowballCount = 0;
+var hutWoodCount = 0;
+var hutSnowballTodo = 0;
+var hutWoodTodo = 0;
+var hutShake = 0;
+
+BMAP* bmpSmoke = "rauch2.tga";
+SOUND* sndTransferHut = "present_counter.OGG";
+SOUND* sndLoseHut = "present_counter.OGG";
+SOUND* sndHitHut = "present_counter.OGG";
 
 void fade_p(PARTICLE* p);
 void smoke_p(PARTICLE* p);
+void hut_event();
+void hut_count();
+void hut_hit();
+
 
 void fade_p(PARTICLE* p)
 {
@@ -13,12 +27,10 @@ void fade_p(PARTICLE* p)
 	p.size = minv(p.skill_b, p.size+time_step*5);
 }
 
-BMAP* bmp_smoke = "rauch2.tga";
-
 void smoke_p(PARTICLE* p)
 {
 	p.skill_a = 3;
-	p.bmap = bmp_smoke;
+	p.bmap = bmpSmoke;
 	p.lifespan = 1200+random(600);
 	p.skill_b = 20 + random(15);
 	p.size = 0;
@@ -26,6 +38,115 @@ void smoke_p(PARTICLE* p)
 	vec_scale(p.vel_x, 0.4);
 	set(p, TRANSLUCENT | MOVE/* | BRIGHT*/);
 	p.event = fade_p;
+}
+
+void hut_event()
+{
+	if (event_type == EVENT_IMPACT && you != NULL)
+	{
+		if (your->ENTITY_TYPE == GOBLIN)
+		{
+			hut_hit();
+			if (hutWoodCount <= 0)
+			{
+				set(my, is_empty);
+			}	
+		}
+		
+	}
+	
+	if (event_type == EVENT_TRIGGER && you != NULL)
+	{
+		if (you == player)
+		{
+			hutWoodTodo += woodCount;
+			woodCount = 0;
+
+			hutSnowballTodo += snowballCount;
+			snowballCount = 0;
+		}
+	}
+}
+
+void hut_count()
+{
+	var added = 0;
+		
+	while (!is(my, is_empty))
+	{
+		added = 0;
+		if (hutWoodTodo > 0)
+		{
+			hutWoodCount++;
+			hutWoodTodo--;	
+			added = 1;		
+		}
+	
+		if (hutWoodTodo < 0)
+		{
+			hutWoodCount--;
+			hutWoodTodo++;	
+			added = -1;		
+		}
+	
+		if (hutSnowballTodo > 0)
+		{
+			hutSnowballCount++;
+			hutSnowballTodo--;	
+			added = 1;		
+		}	
+		
+		if (hutSnowballTodo < 0)
+		{
+			hutSnowballCount--;
+			hutSnowballTodo++;	
+			added = -11;		
+		}	
+		
+		if (added > 0)
+		{
+			snd_play(sndTransferHut, soundVolume, 0);
+			//show panel	
+		}
+		
+		if (added < 0)
+		{
+			snd_play(sndLoseHut, soundVolume, 0);
+			//show panel	
+		}
+		
+		wait (-0.2);
+		//hide panel
+	}
+}
+
+void hut_hit()
+{
+	VECTOR vecPos;
+	VECTOR vecTemp;
+	
+	snd_play(sndHitHut, soundVolume, 0);
+
+	if (hutShake > 0)
+	{
+		hutShake = 0;
+		return;
+	}	
+
+	vec_set(&vecPos, &my->x);
+	while(hutShake < HIT_TIME)
+	{
+		vec_randomize(&vecTemp, 100);
+		vec_normalize(&vecTemp, 20);
+		vec_add(&my->x, &vecTemp);
+		my->z = vecPos.z;
+		wait(-0.15);
+		vec_set(&my->x, &vecPos);
+		wait(-0.05);
+		hutShake += 0.2;
+	}
+	hutShake = 0;
+	hutWoodCount = maxv(hutWoodCount - your->ATTACK_POWER, 0);
 }
 
 action smoke()
@@ -52,6 +173,18 @@ action woodlog()
 
 action hut()
 {
+	my->trigger_range = 200;
+	my->emask |= ENABLE_TRIGGER | ENABLE_IMPACT;
+	my->event = hut_event;	
+	hut_count();
+	while(!is(my, is_empty))
+	{
+		wait (1);
+	}
+	
+	//game over
+	error("Nun.. jetzt hab ich wohl verkackt");
 }
+
 
 #endif
